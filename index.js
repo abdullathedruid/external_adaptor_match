963be29d56c5562d12be7120f6d7cb146e5934b3
Abdulla <abdulla.alkamil@gmail.com>
2021-04-09 23:58:36 +0100

    single_adaptor

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const { Requester, Validator } = require('@chainlink/external-adapter')
-
+const { Requester, Validator, AdapterError } = require('@chainlink/external-adapter')
 // Define custom error scenarios for the API.
 // Return true for the adapter to retry.
 const customError = (data) => {
@@ -12,48 +11,80 @@ const customError = (data) => {
 // with a Boolean value indicating whether or not they
 // should be required.
 const customParams = {
-  base: ['base', 'from', 'coin'],
-  quote: ['quote', 'to', 'market'],
-  endpoint: false
+  matchid: ['matchid'],
+  endpoint: ['endpoint']
+}
+
+function validateResultString (data) {
+  if (typeof data === 'undefined') {
+    const error = 'Result could not be found in path'
+    console.log(error)
+    throw new AdapterError(error)
+  }
+  if (data === '') {
+    const error = 'Invalid result'
+    console.log(error)
+    throw new AdapterError(error)
+  }
+  return data
 }
 
 const createRequest = (input, callback) => {
   // The Validator helps you validate the Chainlink request data
   const validator = new Validator(callback, input, customParams)
   const jobRunID = validator.validated.id
-  const endpoint = validator.validated.data.endpoint || 'price'
-  const url = `https://min-api.cryptocompare.com/data/${endpoint}`
-  const fsym = validator.validated.data.base.toUpperCase()
-  const tsyms = validator.validated.data.quote.toUpperCase()
+  const matchid = validator.validated.data.matchid
+  const endpoint = validator.validated.data.endpoint
+  const base_url = 'https://api.football-data.org/v2/'
+  var final_url = ''
 
-  const params = {
-    fsym,
-    tsyms
+  const headers = {
+    'X-Auth-Token': '3263b9d91c754a82af65d75ce2c86474'
   }
 
-  // This is where you would add method and headers
-  // you can add method like GET or POST and add it to the config
-  // The default is GET requests
-  // method = 'get' 
-  // headers = 'headers.....'
-  const config = {
-    url,
-    params
+  switch (endpoint) {
+    case 'outcome':
+      final_url = base_url + `matches/${matchid}`
+      console.log(final_url)
+      const params = {}
+      const config = {
+        url: final_url,
+        params,
+        headers
+      }
+      // The Requester allows API calls be retry in case of timeout
+      // or connection failure
+      Requester.request(config, customError)
+        .then(response => {
+          // It's common practice to store the desired value at the top-level
+          // result key. This allows different adapters to be compatible with
+          // one another.
+          const output = validateResultString(Requester.getResult(response.data, ['match','score','winner']))
+          switch (output) {
+            case "HOME_TEAM":
+              response.data.result = 1;
+              break;
+            case "DRAW":
+              response.data.result = 2;
+              break;
+            case "AWAY_TEAM":
+              response.data.result = 3;
+              break;
+            default:
+              response.data.result = 0;
+          }
+
+          callback(response.status, Requester.success(jobRunID, response))
+        })
+        .catch(error => {
+          callback(500, Requester.errored(jobRunID, error))
+        })
+    break;
+
+    default:
+      console.log('invalid parameter')
   }
 
-  // The Requester allows API calls be retry in case of timeout
-  // or connection failure
-  Requester.request(config, customError)
-    .then(response => {
-      // It's common practice to store the desired value at the top-level
-      // result key. This allows different adapters to be compatible with
-      // one another.
-      response.data.result = Requester.validateResultNumber(response.data, [tsyms])
-      callback(response.status, Requester.success(jobRunID, response))
-    })
-    .catch(error => {
-      callback(500, Requester.errored(jobRunID, error))
-    })
 }
 
 // This is a wrapper to allow the function to work with
